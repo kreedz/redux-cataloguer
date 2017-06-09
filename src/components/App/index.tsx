2f8e1b7448934f9f53b9a1cdc71cad7b501aca2a
kreedz <kreedz@users.noreply.github.com>
2017-06-09 18:43:40 +0500

    move components from App to Cataloguer

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,28 +1,9 @@
 import React, { SFC } from 'react';
-import { Col, Grid, Row } from 'react-bootstrap';
 
 import Cataloguer from 'containers/Cataloguer';
 
-import Add from 'components/Add';
-import Filter from 'components/Filter';
-import Photos from 'components/Photos';
-import Search from 'components/Search';
-
-import styles from './styles.css';
-
 const App: SFC<any> = () => (
-  <Grid className={styles.cataloguer}>
-    <Row className={styles.rowPadding}>
-      <Search />
-      <Add />
-    </Row>
-    <Row className={styles.rowPadding}>
-      <Filter />
-    </Row>
-    <Row className={styles.rowPadding}>
-      <Photos />
-    </Row>
-  </Grid>
+  <Cataloguer />
 );
 
 export default App;
--- src/containers/Cataloguer/index.tsx
+++ src/containers/Cataloguer/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import { Col, Grid, Row } from 'react-bootstrap';
+
+import Add from 'components/Add';
+import Filter from 'components/Filter';
+import Photos from 'components/Photos';
+import Search from 'components/Search';
+
+import styles from './styles.css';
+
 class Cataloguer extends React.Component<any, any> {
   render() {
     return (
-      <div>Cataloguer!</div>
+      <Grid className={styles.cataloguer}>
+        <Row className={styles.rowPadding}>
+          <Search />
+          <Add />
+        </Row>
+        <Row className={styles.rowPadding}>
+          <Filter />
+        </Row>
+        <Row className={styles.rowPadding}>
+          <Photos />
+        </Row>
+      </Grid>
     );
   }
 }
