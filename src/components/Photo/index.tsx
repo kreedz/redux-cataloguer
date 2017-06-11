332c62095dbe25954d63440b0dde01fbeaed35b5
kreedz <kreedz@users.noreply.github.com>
2017-06-11 17:25:41 +0500

    Photo detailed layout

--- src/components/Photo/index.tsx
+++ src/components/Photo/index.tsx
@@ -1,13 +1,31 @@
 import React, { SFC } from 'react';
-import { Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import { RouteComponentProps } from 'react-router-dom';
 
-import styles from 'styles/styles.css';
+import globalStyles from 'styles/styles.css';
+import styles from './styles.css';
+
+const img = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAQMAAAAlPW0i
+            AAAABlBMVEUAAAD///+l2Z/dAAAAM0lEQVR4nGP4/5/h/1+G/58ZDrAz3D/McH8yw83
+            NDDeNGe4Ug9C9zwz3gVLMDA/A6P9/AFGGFyjOXZtQAAAAAElFTkSuQmCC`;
 
 const Photo: SFC<RouteComponentProps<any>> = props => (
   <div>
-    <Row className={styles.rowPadding}>
-      Hello {props.match.params.imgid}
+    <Row className={globalStyles.rowPadding}>
+      <Col xs={4} xsOffset={4}>
+        <div className={styles.photo}>
+          <div className={styles.photoData}>
+            <div className={styles.photoView}>
+              <img src={img} />
+            </div>
+            Date: {props.match.params.imgid}
+            <br />
+            Likes: 1
+            <br />
+            Description: 123
+          </div>
+        </div>
+      </Col>
     </Row>
   </div>
 );
