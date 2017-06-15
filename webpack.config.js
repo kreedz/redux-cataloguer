20510b886f1fa1aa5e940f2cdbbded200c50c4e6
kreedz <kreedz@users.noreply.github.com>
2017-06-15 12:14:57 +0500

    webpack: file-loader filename

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: 'static/[name].[ext]',
+              name: '[name].[ext]',
             }
           }
         ]
