bda4409aed45f9f6e14100894ec4dd5a11206b20
kreedz <kreedz@users.noreply.github.com>
2017-06-16 20:19:36 +0500

    Add: default input[type=file] text changed

--- src/components/Add/index.tsx
+++ src/components/Add/index.tsx
@@ -1,16 +1,18 @@
 import React, { SFC } from 'react';
 import { Button, Col, Row } from 'react-bootstrap';
 
+import styles from './styles.css';
+
 interface IAddProps {
   handleImageChange: (event: any) => void;
 }
 
 const Add: SFC<IAddProps> = props => (
   <Col xs={4} xsOffset={4} className="text-center">
-    <input
-      type="file"
-      onChange={props.handleImageChange}
-    />
+  <div>
+     <label htmlFor={styles.files} className="btn">Select Image</label>
+     <input id={styles.files} type="file" onChange={props.handleImageChange}/>
+   </div>
   </Col>
 );
 
