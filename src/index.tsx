a795de7950b2c98a0425e3beda0c8620ca8d099f
kreedz <kreedz@users.noreply.github.com>
2017-12-11 14:46:36 +0500

    fix package versions, remove unnecessary import

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import { BrowserRouter } from 'react-router-dom';
-
 import App from 'components/App';
 import store from 'store';
 
